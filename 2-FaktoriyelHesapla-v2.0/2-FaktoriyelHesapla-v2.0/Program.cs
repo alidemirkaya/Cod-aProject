101a491d34ffb5ad14185a18786fb49d59fa1373
Ali Demirkaya <ademirkaya@industryolog.com>
2018-04-25 01:53:38 +0300

    Add files via upload

---
+++ 2-FaktoriyelHesapla-v2.0/2-FaktoriyelHesapla-v2.0/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_FaktoriyelHesapla_v2._0
+{
+    class Program
+    {
+        public static int FaktoriyelHesapla(int sayi)
+        {
+            int faktoriyel = 1;
+            for (int i = 1; i <= sayi; i++)
+            {
+                faktoriyel = faktoriyel * i;
+            }
+            return faktoriyel;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktöriyeli Hesaplanacak sayıyı giriniz ..");
+            int sayi = int.Parse(Console.ReadLine());
+            int faktoriyel = FaktoriyelHesapla(sayi);          
+            Console.WriteLine(sayi + " sayısının Faktöriyeli : " + faktoriyel);
+            Console.ReadKey();
+        }
+    }
+}
