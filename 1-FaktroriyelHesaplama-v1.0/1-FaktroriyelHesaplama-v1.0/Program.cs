2ca06b4f621d72e80ba599e5cfac6d539e327032
Ali Demirkaya <ademirkaya@industryolog.com>
2018-04-24 22:51:58 +0300

    Add files via upload

---
+++ 1-FaktroriyelHesaplama-v1.0/1-FaktroriyelHesaplama-v1.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_FaktroriyelHesaplama_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktöriyeli Hesaplanacak sayıyı giriniz ..");
+            int sayi =int.Parse(Console.ReadLine());
+            int faktoriyel = 1;
+            for(int i = 1; i <= sayi; i++)
+            {
+                faktoriyel = faktoriyel * i;
+            }
+            Console.WriteLine(sayi + " sayısının Faktöriyeli : " + faktoriyel);
+            Console.ReadKey();
+        }
+    }
+}
