75f3fe6515947160aec46b5c5f1d1675e9ca629c
Ali Demirkaya <ademirkaya@industryolog.com>
2018-04-28 20:12:45 +0300

    Add files via upload

---
+++ 9-1-N-Soruları-v4.0/9-1-N-Soruları-v4.0/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9_1_N_Soruları_v4._0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            if (int.Parse(textBox1.Text) >= int.Parse(textBox2.Text))
+            {
+                MessageBox.Show("Başlangıç Sayısı Bitiş Sayısından Büyük Olamaz", "Uyarı");
+            }
+            else
+            {
+                int sayi1 = int.Parse(textBox1.Text);
+                int sayi2 = int.Parse(textBox2.Text);
+                int toplam = 0;
+                if (radioButton1.Checked == true)
+                {
+                    for (int i = sayi1; i < sayi2; i++)
+                    {
+                        if ((i % 2) != 0)
+                        {
+                            toplam = toplam + i;
+                            listBox1.Items.Add(i);
+                        }
+                    }
+                }
+                else if (radioButton2.Checked == true)
+                {
+                    for (int i = sayi1; i < sayi2; i++)
+                    {
+                        if ((i % 2) == 0)
+                        {
+                            toplam = toplam + i;
+                            listBox1.Items.Add(i);
+                        }
+                    }
+                }
+                textBox3.Text =Convert.ToString(listBox1.Items.Count);
+                textBox4.Text = Convert.ToString(toplam);
+            }
+        }
+    }
+}
---
+++ 9-1-N-Soruları-v4.0/9-1-N-Soruları-v4.0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _9_1_N_Soruları_v4._0
+{
+    static class Program
+    {
+        /// <summary>
+        /// Uygulamanın ana girdi noktası.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
