ee18360c08d20a66a8a59136da68479991b8d2d6
Ali Demirkaya <ademirkaya@industryolog.com>
2018-04-28 20:25:14 +0300

    Add files via upload

---
+++ 10-CarpimTablosu-v1.0/10-CarpimTablosu-v1.0/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_CarpimTablosu_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 1; i <= 10; i++)
+            {
+                for(int j = 1; j <= 10; j++)
+                {
+                    Console.Write((i*j)+"  ");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+        
+    }
+}
