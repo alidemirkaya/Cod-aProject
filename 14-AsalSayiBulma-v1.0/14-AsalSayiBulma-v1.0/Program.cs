622ca47d8998a60c9d5e2684587594efab0c907d
Ali Demirkaya <ademirkaya@industryolog.com>
2018-04-30 00:58:39 +0300

    Add files via upload

---
+++ 14-AsalSayiBulma-v1.0/14-AsalSayiBulma-v1.0/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_AsalSayiBulma_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            int kalan;
+            int[] Asal;
+            Console.WriteLine("Hangi Sayıya Kadar Asal Sayılar Bulunsun : ");
+            int sayi = int.Parse(Console.ReadLine());
+            for(int i = 2; i < sayi; i++)
+            {
+                kalan = 1;
+                for(int j = 2; j < (i - 1); j++)
+                {
+                    if (i % j == 0)
+                    {
+                        kalan = 0;
+                        break;
+                    }
+                }
+                if (kalan == 1)
+                {                   
+                    b = b + 1;
+                    Console.WriteLine(i);
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
