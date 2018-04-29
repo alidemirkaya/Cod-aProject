e377969934aac1d8c787d1eff14b4dd117768ce9
Ali Demirkaya <ademirkaya@industryolog.com>
2018-04-30 00:31:47 +0300

    Add files via upload

---
+++ 13-DiktortgenCizimi-v1.0/13-DiktortgenCizimi-v1.0/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_DiktortgenCizimi_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dikdörgenin genişliğini Giriniz : ");
+            int sayi1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Diktörgenin yüksekliğini Giriniz : ");
+            int sayi2 = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayi2 + 1; i++)
+            {
+                for(int j=1;j<= sayi1 + 1; j++)
+                {
+                    if ((i == 1 || i==sayi2+1) && j <= sayi1 + 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else if((j==1 || j==sayi1+1) && i<= sayi2 + 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
