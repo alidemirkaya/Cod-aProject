30c8b6c7a8e6a960d85e63ac5addc7b69bd34b1b
Ali Demirkaya <ademirkaya@industryolog.com>
2018-04-30 02:14:14 +0300

    Add files via upload

---
+++ 18-EskenarUcgenOlustur-v1.0/18-EskenarUcgenOlustur-v1.0/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _18_EskenarUcgenOlustur_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("K sayısını Giriniz : ");
+            int k = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= (k - 1); i++)
+            {
+                for (int j = 1; j <= (k - i); j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int m = 1; m <= (2 * i + 1); m++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            for (int i = (k - 1); i >= -0; i--)
+            {
+                for (int j = 0; j <= (k - i); j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int m = 1; m <= (2 * i + 1); m++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
