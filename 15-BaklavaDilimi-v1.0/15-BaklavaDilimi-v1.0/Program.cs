85adb43fa9ed6b7ceb9eacf0162e6f402b8ddac8
Ali Demirkaya <ademirkaya@industryolog.com>
2018-04-30 01:15:32 +0300

    Add files via upload

---
+++ 15-BaklavaDilimi-v1.0/15-BaklavaDilimi-v1.0/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15_BaklavaDilimi_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("N değerini giriniz : ");
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)s
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            for (int k = n; k >= 1; k--)
+            {
+                for(int t = 1; t <= k; t++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
