662d52ba70c472ad2c31a0e24fe38588a5f54385
Ali Demirkaya <ademirkaya@industryolog.com>
2018-04-30 02:11:43 +0300

    Add files via upload

---
+++ 16-UcgenOlusturma-v1.0/16-UcgenOlusturma-v1.0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_UcgenOlusturma_v1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("K sayısını Giriniz : ");
+            int k = int.Parse(Console.ReadLine());
+            for(int i = 0; i <= (k - 1); i++)
+            {
+                for(int j = 1; j <= (k - i);j++)
+                {
+                    Console.Write(" ");
+                }
+                for(int m=1; m <= (2 * i + 1); m++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
